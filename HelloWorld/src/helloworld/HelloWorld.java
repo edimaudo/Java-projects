41e824c578a1085c0fff0edcdf1bede91fa9e9da
Edima <edimaudo@gmail.com>
2020-01-05 11:10:52 -0500

    Create HelloWorld.java

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * The program prints Hello, World! in the command line
+ * 
+ * 
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author edima
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, World");
+    }
+    
+}
