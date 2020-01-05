30e1e1690fa20e4a5603da62d03c29e5ddb65ed1
Edima <edimaudo@gmail.com>
2020-01-05 11:17:18 -0500

    Create HelloWorld.java

---
+++ Computer_programming_with_a_purpose/HelloWorld.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:              Ed
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2020
+ **************************************************************************** */
+
+ public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+    }
+}
