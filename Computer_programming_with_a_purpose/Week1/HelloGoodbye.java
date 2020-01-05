0832f965d2f97836bd309c5239f0e81cc58ced03
Edima <edimaudo@gmail.com>
2020-01-05 11:18:08 -0500

    Create HelloGoodbye.java

---
+++ Computer_programming_with_a_purpose/Week1/HelloGoodbye.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name:              Ed
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2020
+ **************************************************************************** */
+
+ public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello " + arg[0] + " and " + arg[1]);
+        System.out.println("Goodbye " + arg[1] + " and " + arg[0]);
+
+    }
+}
